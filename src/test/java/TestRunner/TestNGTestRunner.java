90e6a2adec9f89918d76bcfd0e9f355006759e13
veerababuch7569@gmail.com <Veera.94@>
2025-11-15 11:49:47 +0530

    Initial Commit
    Initial Commit

--- src/test/java/TestRunner/TestNGTestRunner.java
+++ src/test/java/TestRunner/TestNGTestRunner.java
@@ -2,20 +2,15 @@ package TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 @CucumberOptions(
-    features = "src/test/resources/Features",  
+    features = "src/test/resources/Features",
     glue = {"StepDefinitions", "Utilities"},
     plugin = {
-        "pretty",
-        "html:target/cucumber-reports/cucumber.html",
-        "json:target/cucumber-reports/cucumber.json",
         "rerun:target/rerun.txt"
     },
     monochrome = true,
     dryRun = false,
     tags = "@AddEmployeeScenario"
 )
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
-    
-}
+public class TestNGTestRunner extends AbstractTestNGCucumberTests { }
